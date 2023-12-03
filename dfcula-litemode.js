8ba8de1044c80134447f31931c80c0f4dd3ce276
SayCula <98538872+SayCula@users.noreply.github.com>
2023-12-03 22:29:04 +0100

    deletes all

--- dfcula-litemode.js
+++
@@ -1,4 +0,0 @@
-function litemode() {
-  document.body.style.backgroundColor = "#f3f3f3";
-  document.body.style.backgroundImage = "url('https://www.mediafire.com/convkey/9181/8d59y4ufyizk2vl9g.jpg')";
-}
