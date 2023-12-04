ea0469029dae85741b6b40fa8a59d4fa5b3cf6dc
SayCula <98538872+SayCula@users.noreply.github.com>
2023-12-04 15:31:17 +0100

    add label

--- MinecraftMods/scripts/home.js
+++ MinecraftMods/scripts/home.js
@@ -3,14 +3,16 @@ let userHTML = '';
 mods.forEach((mod) => {
   userHTML += ` 
   <div class="checkbox-container">
-    <input type="checkbox" id="${mod.id}" 
-    data-mod-name="${mod.name}"
-    data-mod-version="${mod.version}"
-    data-mod-luncher="${mod.luncher}">
-    <h3>Mod Name : ${mod.name}</h3>
-    <h3>Mod Version : ${mod.version}</h3>
-    <h3>Mod Luncher : ${mod.luncher}</h3>
-  </div>
+    <label for="${mod.id}">
+        <input type="checkbox" id="${mod.id}" 
+        data-mod-name="${mod.name}"
+        data-mod-version="${mod.version}"
+        data-mod-luncher="${mod.luncher}">
+        <h3>Mod Name : ${mod.name}</h3>
+        <h3>Mod Version : ${mod.version}</h3>
+        <h3>Mod Luncher : ${mod.luncher}</h3>
+    </label>
+</div>
   `;
 })
 document.querySelector('.mods-lists').innerHTML = userHTML;
