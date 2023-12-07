0e762903bc4a29a79aa124c6724d7bad329ee2a2
SayCula <98538872+SayCula@users.noreply.github.com>
2023-12-07 15:34:21 +0100

    remove all

--- scripts/currentDate.js
+++
@@ -1,6 +0,0 @@
-const currentDate = new Date();
-const year = currentDate.getFullYear();
-const month = currentDate.getMonth() + 1;
-const day = currentDate.getDate();
-let dateElement = document.querySelector('.dateElement');
-dateElement.innerHTML= `Effective Date: ${month}/${day}/${year}.`
