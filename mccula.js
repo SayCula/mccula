ca228f347c080b6603e2c44c79a8553eba668bc6
SayCula <98538872+SayCula@users.noreply.github.com>
2023-12-18 02:40:55 +0100

    delet

---
+++ mccula.js
@@ -0,0 +1,24 @@
+const mcCula = [
+    {
+        id: 1,
+        name: 'NameMod',
+        class: 'mod',
+        version: '0.00.0',
+        Loaders: 'Fabric',
+        link: 'https://www.mediafire.com/file/lwp9l9ao3gmelcg/VoxelMap__1.16.4.zip/file',
+    },
+    {
+        id: 2,
+        name: 'NameResourcePacks',
+        class: 'ResourcePacks',
+        version: '0.00.0',
+        link: 'https://www.mediafire.com/file/xnpjrkiyork70g3/Dark_Theme_v_ec_1.15.%252B.zip/file'
+    },
+    {
+        id: 3,
+        name: 'NameWorld',
+        class: 'World',
+        version: '0.00.0',
+        link: 'https://www.mediafire.com/file/tezt829jvrvskee/SkyBlock2.1.rar/file'
+    },
+]
