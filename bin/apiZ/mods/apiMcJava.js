527f8ede96a79fc13fdb24f3b3a5ebf766c380df
SayCula <98538872+SayCula@users.noreply.github.com>
2024-01-08 16:51:41 +0100

    Update apiMcJava.js

--- bin/apiZ/mods/apiMcJava.js
+++ bin/apiZ/mods/apiMcJava.js
@@ -3,10 +3,10 @@ export const apiMcJava = [
     {
         id: 7,
         name: 'OptiFine Jei JourneyMap',
-        image: "/img/avatars/McCula.zip.png",
+        image: "./img/avatars/McCula.zip.png",
         class:'mod',
         gamename: 'javaEdition',
-        link: '/downloads/mods/McCula.zip/',
+        link: './downloads/mods/McCula.zip/',
     },
     {
         id: 6,
@@ -14,7 +14,7 @@ export const apiMcJava = [
         image: "https://media.forgecdn.net/avatars/595/863/637971183022756912.gif",
         class:'World',
         gamename: 'javaEdition',
-        link: '/downloads/worlds/oneblockoriginal',
+        link: './downloads/worlds/oneblockoriginal',
     },
     {
         id: 5,
@@ -22,7 +22,7 @@ export const apiMcJava = [
         image: "https://media.forgecdn.net/avatars/375/56/637549610342642859.png",
         class:'mod',
         gamename: 'javaEdition',
-        link: '/downloads/mods/sophisticatedbackpacks',
+        link: './downloads/mods/sophisticatedbackpacks',
     },
     {
         id: 4,
@@ -30,23 +30,23 @@ export const apiMcJava = [
         image: "https://media.forgecdn.net/avatars/9/144/635421614078544069.png",
         class:'mod',
         gamename: 'javaEdition',
-        link: '/downloads/mods/journeymap',
+        link: './downloads/mods/journeymap',
     },
     {
         id: 3,
         name: 'OptiFine',
-        image: "/img/avatars/McCula.zip.png",
+        image: "./img/avatars/McCula.zip.png",
         class:'mod',
         gamename: 'javaEdition',
-        link: '/downloads/mods/McCula.zip/',
+        link: './downloads/mods/McCula.zip/',
     },
     {
         id: 2,
         name: 'Skyblock',
-        image: "/img/avatars/skyblocclassic.png",
+        image: "./img/avatars/skyblocclassic.png",
         class:'World',
         gamename: 'javaEdition',
-        link: '/downloads/worlds/skyblocclassic/',
+        link: './downloads/worlds/skyblocclassic/',
     },
     {
         id: 1,
@@ -54,7 +54,7 @@ export const apiMcJava = [
         image: "https://media.forgecdn.net/avatars/29/69/635838945588716414.jpeg",
         class:'mod',
         gamename: 'javaEdition',
-        link: '/downloads/mods/justenoughitems/',
+        link: './downloads/mods/justenoughitems/',
     },
     
 
